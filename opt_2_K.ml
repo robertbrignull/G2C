798547902c8c616ce062e8acaeb159012be15b82
Robert Brignull <robert.brignull@gmail.com>
2014-10-24 17:18:08 +0100

    Couple more optimisations to K.

--- opt_2_K.ml
+++ opt_2_K.ml
@@ -35,6 +35,41 @@ let replace_id source target expr =
 
   in expr_replace_id expr
 
+let count_id target expr =
+  let rec expr_count_id = function
+    | Let (id, decl, expr) ->
+        if id = target then
+          0
+        else
+          decl_count_id decl +
+          expr_count_id expr
+    | If (test, then_expr, else_expr) ->
+        value_count_id test +
+        expr_count_id then_expr +
+        expr_count_id else_expr
+    | App (expr, args) ->
+        value_count_id expr +
+        List.fold_left (+) 0 (List.map value_count_id args)
+    | Halt value ->
+        value_count_id value
+
+  and decl_count_id = function
+    | Value value ->
+        value_count_id value
+    | Op (op, args) ->
+        List.fold_left (+) 0 (List.map value_count_id args)
+
+  and value_count_id = function
+    | (Bool b, type_c) ->  0
+    | (Num x, type_c) -> 0
+    | (Id id, type_c) ->
+        if id = target then 1
+        else 0
+    | (Lambda (args, expr), type_c) ->
+        expr_count_id expr
+
+  in expr_count_id expr
+
 let rec optimise expr_in =
   let expr_top = ref expr_in in
   let changed = ref true in
@@ -59,6 +94,19 @@ let rec optimise expr_in =
     | Let (id_1, Value (Id id_2, type_c), expr) -> begin
         changed := true;
         replace_id id_1 (Id id_2) expr
+      end
+
+    (* Remove inline lambdas, replace them by lets *)
+    | App ((Lambda (l_args, l_expr), type_c), f_args) -> begin
+        changed := true;
+        let args = List.combine l_args f_args in
+        List.fold_right (fun ((arg_name, arg_type), arg_value) rest -> Let (arg_name, Value arg_value, rest)) args l_expr
+      end
+
+    (* Remove lets where the value is never used *)
+    | Let (id, value, expr) when count_id id expr = 0 -> begin
+        changed := true;
+        expr
       end
 
     (* End of optimisations, just recurse *)
